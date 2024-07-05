f4bbf44147676f96f0e7d0532f13d766b5451bbb
Samarjeet Singh <samarjeetsingh@Samarjeets-MacBook-Pro.local>
2024-07-05 17:31:41 +0530

    anomaly model

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.ensemble import IsolationForest
-import joblib
+from sklearn2pmml import PMMLPipeline, sklearn2pmml
 import boto3
 
 # Load the dataset
@@ -15,24 +15,25 @@ data['motion'] = data['motion'].astype(bool)
 # Select relevant features
 features = ['temp', 'co', 'humidity', 'lpg', 'smoke']
 
-# Train an Isolation Forest model for each feature and serialize them
+aws_access_key_id = "my_key"
+aws_secret_access_key = "my_access_key"
+aws_region = 'us-east-1'
+bucket_name = 'telemetry-model'  # Change this to your S3 bucket name
+
 for feature in features:
     model = IsolationForest(contamination=0.01)  # Adjust contamination rate as needed
-    model.fit(data[[feature]])
+    pipeline = PMMLPipeline([("estimator", model)])
+    pipeline.fit(data[[feature]])
 
-    # Serialize the model
-    model_filename = f'{feature}_isolation_forest_model.pkl'
-    joblib.dump(model, model_filename)
+    # Serialize the model to PMML
+    pmml_filename = f'{feature}_isolation_forest_model.pmml'
+    sklearn2pmml(pipeline, pmml_filename)
 
-    aws_access_key_id = "my_key"
-    aws_secret_access_key = "my_access_key"
-    aws_region = 'us-east-1'
-    # Upload the serialized model to S3
+    # Upload the PMML model to S3
     s3 = boto3.client('s3',
                       aws_access_key_id=aws_access_key_id,
                       aws_secret_access_key=aws_secret_access_key,
                       region_name=aws_region)
-    bucket_name = 'telemetry-model'  # Change this to your S3 bucket name
-    s3.upload_file(model_filename, bucket_name, model_filename)
+    s3.upload_file(pmml_filename, bucket_name, pmml_filename)
 
-    print(f'Model for {feature} saved to S3 bucket {bucket_name} as {model_filename}')
+    print(f'Model for {feature} saved to S3 bucket {bucket_name} as {pmml_filename}')
